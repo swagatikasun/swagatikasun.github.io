4a3007a17f205c48ff6b0333c956114ef898c2a3
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2022-11-12 18:32:42 +0530

    Added Dynamic Theme

--- src/components/theme/index.jsx
+++ src/components/theme/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './index.css';
 
 const Component = () => {
@@ -8,8 +8,22 @@ const Component = () => {
         else if (mode === 1) document.querySelector('meta[name="theme-color"]').content = '#111';
     };
 
+    const toDark = (style, theme) => {
+        // Change to Dark Mode
+        style.setAttribute('href', theme);
+        changeMetaTheme(1);
+        document.cookie = `theme=1;samesite=strict;expires=Fri, 31 Dec 9999 23:59:59 GMT`;
+    };
+
+    const toLight = (style, theme) => {
+        // Change to Light Mode
+        style.setAttribute('href', theme);
+        changeMetaTheme(0);
+        document.cookie = `theme=0;samesite=strict;expires=Fri, 31 Dec 9999 23:59:59 GMT`;
+    };
+
     // Change Theme
-    const changeTheme = () => {
+    const changeTheme = (auto = null) => {
         const style = document.querySelector('link[rel="newest stylesheet"]');
 
         // get current theme
@@ -21,16 +35,16 @@ const Component = () => {
         // resolve dark theme path
         const dark = `${process.env.PUBLIC_URL}/css/DarkTheme.css`;
 
-        if (current === light) {
-            // Change to Dark Mode
-            style.setAttribute('href', dark);
-            changeMetaTheme(1);
-            document.cookie = `theme=1;samesite=strict;expires=Fri, 31 Dec 9999 23:59:59 GMT`;
-        } else if (current === dark) {
-            // Change to Light Mode
-            style.setAttribute('href', light);
-            changeMetaTheme(0);
-            document.cookie = `theme=0;samesite=strict;expires=Fri, 31 Dec 9999 23:59:59 GMT`;
+        if (auto === null) {
+            if (current === light) {
+                toDark(style, dark);
+            } else if (current === dark) {
+                toLight(style, light);
+            }
+        } else if (auto === 0) {
+            toLight(style, light);
+        } else if (auto === 1) {
+            toDark(style, dark);
         }
     };
 
@@ -45,6 +59,26 @@ const Component = () => {
     //             : undefined;
     //     if (theme && theme === '1') changeTheme();
     // });
+
+    // Auto theming based on timexone. Day = Light and Night = Dark
+    const dynamicMode = () => {
+        const hour = new Date().getHours();
+
+        if (hour > 5 && hour < 18) {
+            // Change to Dark Mode
+            changeTheme(0);
+            changeMetaTheme(0);
+        } else {
+            // Change to Light Mode
+            changeTheme(1);
+            changeMetaTheme(1);
+        }
+    };
+
+    // Execute auto-theming once page is loaded
+    useEffect(() => {
+        dynamicMode();
+    });
 
     return (
         <span>
