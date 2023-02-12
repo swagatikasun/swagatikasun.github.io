1d5de9be65057e65e246fcefb8dfa09126e5355d
Swagatika Sahoo <swagatikasun12@gmail.com>
2023-02-12 11:54:46 +0530

    Fixed problems with email button link

--- src/components/social/email.jsx
+++ src/components/social/email.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Buffer } from 'buffer';
 
-import './email.css';
+// import './email.css';
 
 const Component = ({ address }) => {
     return (
-        <button
-            className="email-button"
-            type="button"
+        <a
+            href="#"
+            // className="email-button"
+            // type="button"
             onClick={() => {
                 const mailtoURL = `mailto:${Buffer.from(address, 'base64').toString()}`;
                 window.open(mailtoURL, '_blank')?.focus();
             }}
         >
             <i className="far fa-envelope-open" />
-        </button>
+        </a>
     );
 };
 
