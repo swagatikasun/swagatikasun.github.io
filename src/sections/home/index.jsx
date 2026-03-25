0dbef3c9a83d2f669fbfd7349e4768665c083c92
Swagatika Sahoo <66073961+swagatikasun@users.noreply.github.com>
2026-03-25 14:55:51 +0530

    Update index.jsx

--- src/sections/home/index.jsx
+++ src/sections/home/index.jsx
@@ -10,7 +10,7 @@ const Component = () => {
         <div className="row home-sec">
             <div className="col-lg-4 col-12 d-lg-block d-none home-bio">
                 <div className="home-img">
-                    <img src={`${process.env.PUBLIC_URL}/static/img/66073961.jpeg`} alt="profile" />
+                    <img src={`${process.env.PUBLIC_URL}/static/img/WhatsApp Image 2026-03-25 at 2.32.09 PM.jpeg`} alt="profile" />
                 </div>
                 <Bio largeMode />
             </div>
